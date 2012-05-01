c1a85f28ed9bf95819e7703e9ebfb861ebfef3f5
Tim Jones <tim@timjones.tw>
2012-05-02 00:26:23 +0800

    Initial commit

---
+++ src/Aether/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  Aether
+//
+//  Created by Tim Jones on 01/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
