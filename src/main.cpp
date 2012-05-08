f2e198974ad0debd76d6198c26cabfcedb881344
Tim Jones <tim@timjones.tw>
2012-05-08 14:07:23 +0800

    Can now write PNG file, finally

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <ostream>
+#include <png.hpp>
 #include "Aether.h"
 
 int main(int argc, char* argv[])
@@ -5,4 +7,15 @@ int main(int argc, char* argv[])
   int pixelWidth = 600;
   int pixelHeight = 400;
   std::vector<Color> renderedSceneData = aetherRenderScene(pixelWidth, pixelHeight);
+  
+  png::image<png::rgb_pixel> image(128, 128);
+	for (size_t y = 0; y < image.get_height(); ++y)
+	{
+		for (size_t x = 0; x < image.get_width(); ++x)
+		{
+			image[y][x] = png::rgb_pixel(x, y, x + y);
+			// non-checking equivalent of image.set_pixel(x, y, ...);
+		}
+	}
+  image.write("output.png");
 }
